e14eff933bd6c6b02d4015bd20089f94fa45f0a3
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-17 15:02:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,27 @@
+
+
+
+
+
+
+
+
+
+
 def create_an_empty_array
-  
+  array1 = []
 end
 
 def create_an_array
-  
+  array1 = ["Spring", "Summer", "Fall", "Winter"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array1 = ["Spring", "Summer", "Fall", "Winter"], element = "Cold")
+  array1 << element
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array1 = ["Spring", "Summer", "Fall", "Winter"], element = "Cold")
+  array1.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
